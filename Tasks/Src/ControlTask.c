cb71e82923f0b626c8c1b94af8f45e53433667a1
Chen Xiaofeng <515318881@qq.com>
2017-12-17 22:21:51 +0800

    一点误写

--- Tasks/Src/ControlTask.c
+++ Tasks/Src/ControlTask.c
@@ -18,8 +18,6 @@
 #define LED_GREEN_ON()    HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin,GPIO_PIN_RESET)
 #define LED_RED_ON()      HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin,GPIO_PIN_RESET)
 
-void HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
-
 uint8_t find_enemy = 0;
 uint16_t enemy_yaw = YAW_OFFSET;
 uint16_t enemy_pitch = PITCH_OFFSET;
